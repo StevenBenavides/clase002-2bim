6feea2195158d657629d11fd3af900bbfe2c739c
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-06-11 12:13:25 -0500

    Segundo

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
 
 class Estudiante(models.Model):
@@ -11,7 +13,20 @@ class Estudiante(models.Model):
     cedula = models.CharField(max_length=30, unique=True)
     edad = models.IntegerField()
 
+
 #representamos los objetos 
     def __str__(self):
-        return f"{self.nombre} - {self.apellido} - {self.cedula} - {self.edad}"
+        return f"{self.nombre} - {self.apellido} - {self.verificar_cedula()} - {self.edad} - {self.obtener_anio() }"
+   
+
+    def obtener_anio(self):
+        obtener_actual = datetime.now().year
+        valor = obtener_actual - self.edad
+        return valor
+    
+    def verificar_cedula(self):
+        primeros_digitos = self.cedula[0:2]
+        if primeros_digitos == "11":
+            return "Loja"
+        return "Otra ciudad"
 
