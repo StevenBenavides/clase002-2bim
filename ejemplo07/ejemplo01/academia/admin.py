599cdf5df8214f4561b401998027c03e63eb4b8f
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-06-11 11:50:35 -0500

    Primero

---
+++ ejemplo07/ejemplo01/academia/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from academia.models import Estudiante
+# Register your models here.
+
+# el admin permite regstar los odelos para gdjando 
+admin.site.register(Estudiante)
+
---
+++ ejemplo07/ejemplo01/academia/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField()
+
+#representamos los objetos 
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido} - {self.cedula} - {self.edad}"
+
